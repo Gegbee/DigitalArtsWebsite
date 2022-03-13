1b832389d2ba4ed7069e404a559ef9977dd1537a
yello12 <brickbeacon123@gmail.com>
2022-03-12 19:00:15 -0800

    Fixed read more

--- app.js
+++ app.js
@@ -1,22 +1,22 @@
 "use strict";
 
-const mobileButton = document.getElementById("mobile-links-show");
-const mobileLinks = document.getElementById("mobile-links");
+// const mobileButton = document.getElementById("mobile-links-show");
+// const mobileLinks = document.getElementById("mobile-links");
 
 const readMore = document.getElementById("read-more");
 
-let showingLinks = false;
+// let showingLinks = false;
 
-mobileButton.addEventListener("click", () => {
- 	showingLinks = !showingLinks;
-   if (showingLinks) {
-      mobileButton.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 60 60" overflow="visible" stroke="black" stroke-width="10" stroke-linecap="round"><line x2="50" y2="50" /><line x1="50" y2="50" /></svg>';
-      mobileLinks.style.opacity = 1;
-   } else {
-      mobileButton.innerHTML = '<svg viewBox="0 0 60 60" width="50" height="50"><rect y="10" width="45" height="5"></rect><rect y="25" width="45" height="5"></rect><rect y="40" width="45" height="5"></rect></svg>';
-      mobileLinks.style.opacity = 0;
-   }
-});
+// mobileButton.addEventListener("click", () => {
+//  	showingLinks = !showingLinks;
+//    if (showingLinks) {
+//       mobileButton.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 60 60" overflow="visible" stroke="black" stroke-width="10" stroke-linecap="round"><line x2="50" y2="50" /><line x1="50" y2="50" /></svg>';
+//       mobileLinks.style.opacity = 1;
+//    } else {
+//       mobileButton.innerHTML = '<svg viewBox="0 0 60 60" width="50" height="50"><rect y="10" width="45" height="5"></rect><rect y="25" width="45" height="5"></rect><rect y="40" width="45" height="5"></rect></svg>';
+//       mobileLinks.style.opacity = 0;
+//    }
+// });
 
 $(document).ready(() => {resizeReadMore()});
 $(document.body).scroll(() => {resizeReadMore()});
@@ -26,7 +26,6 @@ function resizeReadMore() {
    const a = document.body.scrollTop;
    const b = document.body.scrollHeight - document.body.clientHeight;
    const c = a / b;
-   console.log(document.body.clientWidth);
 	if (document.body.clientWidth > 1200 || b === 0 || c > 0.4) {
       // readMore.style.display = "none";
       readMore.style.opacity = 0;
